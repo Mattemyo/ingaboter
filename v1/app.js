9715183f92741979ddbbc795913cd8da6a41e8c6
Mattemyo <matias97marti@gmail.com>
2018-01-21 19:16:52 +0100

    include stockholm parking api and google maps api

--- v1/app.js
+++ v1/app.js
@@ -1,5 +1,6 @@
 $(function() {
   // ============  PARKING API ================= //
+ 
   const weekdayNum = new Date().getDay();
   const weekdays = [
     "söndag",
@@ -11,8 +12,15 @@ $(function() {
     "lördag"
   ];
   // WEEEEEEEEK DAAAAAYS
-  // TODO: Make sure yesterday is safe
   const importantDays = [
+    {
+      key: 'dayBeforeYesterday',
+      weekday: weekdays[weekdayNum === 0 ? 5 : weekdayNum - 2]
+    },
+    {
+      key: "yesterday",
+      weekday: weekdays[weekdayNum === 0 ? 6 : weekdayNum - 1]
+    },
     {
       key: "today",
       weekday: weekdays[weekdayNum]
@@ -31,14 +39,14 @@ $(function() {
   const firstStockUrl =
     "https://openparking.stockholm.se/LTF-Tolken/v1/servicedagar/weekday/";
   const lastStockUrl =
-    "?outputFormat=json&apiKey=31be1dc0-8e91-41ff-b9f3-33fe1208c1d6&maxFeatures=5";
-
+    "?outputFormat=json&apiKey=";
+  const sthmlApiKey = "31be1dc0-8e91-41ff-b9f3-33fe1208c1d6&maxFeatures=1";
   // ====== REQUEST YESTERDAY'S, TODAY'S, AND TOMORROWS INFO ==== //
   importantDays.map(importantDay => {
     console.log(importantDay.weekday);
     $.ajax({
       type: "GET",
-      url: firstStockUrl + importantDay.weekday + lastStockUrl,
+      url: firstStockUrl + importantDay.weekday + lastStockUrl + sthmlApiKey,
       dataType: "jsonp",
       async: false
     }).done(function(data) {
@@ -46,9 +54,6 @@ $(function() {
       appendToUl(data.features, importantDay);
     });
   });
-
-
-
 
   // ====== ADD ELEMENTS TO LIST ===== //
   appendToUl = (data, importantDay) => {
@@ -58,4 +63,17 @@ $(function() {
       );
     });
   };
+
+
+// ============= GOOGLE MAPS API =========== //
+const googleApiKey = "AIzaSyBWagNfi1z8VDkcHSS2dXlzgTPpDNby3Qg";
+
+let map;
+function initMap() {
+  map = new googleApiKey.maps.Map($('#map'), {
+    center: {lat: }
+  })
+}
+
+
 });
